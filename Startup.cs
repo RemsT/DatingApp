65d93cc24504a35666572633b5bd097b5c9317b3
Salmon, Remy <R.Salmon@Medpace.com>
2021-04-12 10:30:11 +0200

    End of Section 3

--- Startup.cs
+++ Startup.cs
@@ -27,6 +27,7 @@ namespace DAtingApp
                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
            services.AddControllers();
+           services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,6 +41,8 @@ namespace DAtingApp
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
